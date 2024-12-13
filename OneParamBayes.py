b4fceb6734dfcc0a5176f0bbd97c1e4023940dd5
Vera Schroeder <schroeder.vera@gmail.com>
2024-12-12 20:02:57 -0600

    Update files

--- OneParamBayes.py
+++ OneParamBayes.py
@@ -34,12 +34,15 @@ import FunctionsForBayes as ffb
 sim_params = [0, 0, -5.9, 0.209] #[gam, lam, b0,b1]
 x = [6, 12, 18, 24, 32, 38, 44, 50]
 #num_trials=50000
-for num_trials in np.arange(10,20010,10):
+postmeans = []
+post3hdi = []
+post97hdi = []
+for num_trials in np.arange(10,20010,50):
     n = np.full(8, num_trials, dtype=int)
     
     
     # Generate sample observed data
-    ydata = ffb.sim_exp_data(sim_params, 100)
+    ydata = ffb.sim_exp_data(sim_params, n)
     yndata = ydata*n
     
     # Define the model
@@ -55,37 +58,39 @@ for num_trials in np.arange(10,20010,10):
         trace = pm.sample(1000, return_inferencedata=True)
     
     # Plot posterior distributions
-    #az.plot_posterior(trace, var_names=["b0"])
+    az.plot_posterior(trace, var_names=["b0"])
     plt.show()    
     # Print a summary of the posterior
     print('----------------------------------------------------------------------')
     print(f'num_trials = {num_trials}')
     print(az.summary(trace, var_names=["b0"]))
+    postmeans.append(float(az.summary(trace)["mean"]))
+    post3hdi.append(float(az.summary(trace)["hdi_3%"]))
+    post97hdi.append(float(az.summary(trace)["hdi_97%"]))
     
-    
-    # rec_params = sim_params.copy()
-    # rec_params[2] =  float(az.summary(trace)["mean"])
-    # lorec_params = sim_params.copy()
-    # lorec_params[2] =  float(az.summary(trace)["hdi_3%"])
-    # hirec_params = sim_params.copy()
-    # hirec_params[2]  =  float(az.summary(trace)["hdi_97%"])
-    # #######
-    # # Plots
+    rec_params = sim_params.copy()
+    rec_params[2] =  float(az.summary(trace)["mean"])
+    lorec_params = sim_params.copy()
+    lorec_params[2] =  float(az.summary(trace)["hdi_3%"])
+    hirec_params = sim_params.copy()
+    hirec_params[2]  =  float(az.summary(trace)["hdi_97%"])
     #######
-    # xfit = np.linspace(6,50,1000)
+    # Plots
+    ######
+    xfit = np.linspace(6,50,1000)
     
-    # ysim = ffb.phi_with_lapses(sim_params,xfit)
-    # yrec = ffb.phi_with_lapses(rec_params,xfit)
-    # loyrec = ffb.phi_with_lapses(lorec_params,xfit)
-    # hiyrec = ffb.phi_with_lapses(hirec_params,xfit)
+    ysim = ffb.phi_with_lapses(sim_params,xfit)
+    yrec = ffb.phi_with_lapses(rec_params,xfit)
+    loyrec = ffb.phi_with_lapses(lorec_params,xfit)
+    hiyrec = ffb.phi_with_lapses(hirec_params,xfit)
     
     
-    # plt.plot(xfit,ysim,label='Original',color='blue')
-    # plt.plot(xfit,yrec,label='Recovered',color='green')
-    # plt.plot(xfit,loyrec,color='green')
-    # plt.plot(xfit,hiyrec,color='green')
-    # plt.scatter(x,ydata, color = 'red')
-    # plt.title(f'Recovering b0 =  {sim_params[2]} from {num_trials} trials')
-    # plt.xlabel(f'Recovered b0~= {rec_params[2]}')
-    # plt.legend()
-    # plt.show()    
+    plt.plot(xfit,ysim,label='Original',color='blue')
+    plt.plot(xfit,yrec,label='Recovered',color='green')
+    plt.plot(xfit,loyrec,color='green')
+    plt.plot(xfit,hiyrec,color='green')
+    plt.scatter(x,ydata, color = 'red')
+    plt.title(f'Recovering b0 =  {sim_params[2]} from {num_trials} trials')
+    plt.xlabel(f'Recovered b0~= {rec_params[2]}')
+    plt.legend()
+    plt.show()    
