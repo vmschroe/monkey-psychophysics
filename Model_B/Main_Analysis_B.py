c99c3bbec02973713eefd2e7096037f83a66c02d
Vera Schroeder <schroeder.vera@gmail.com>
2026-09-15 14:29:01 -0500

    making progress on model B

--- Model_B/Main_Analysis_B.py
+++ Model_B/Main_Analysis_B.py
@@ -39,7 +39,7 @@ exec(open("Build_Model_B.py").read())
 #%% Sample from posteriors
 
 with model_B:
-    trace = pm.sample(draws = 2000, return_inferencedata=True, chains = 4, cores = 1, progressbar=True, idata_kwargs={"log_likelihood": True})   
+    trace = pm.sample(draws = 5000, return_inferencedata=True, chains = 4, cores = 1, progressbar=True, idata_kwargs={"log_likelihood": True})   
 print("FINISHED SAMPLING!")
 
 
@@ -78,7 +78,7 @@ az.plot_trace(trace, var_names=('gam_h', 'mu_gams', 'sig_gams'), coords = {
     'sessions':['05-30'],}, compact=False,  backend_kwargs={"constrained_layout": True})
 
 
-#%% weird joint posterior
+#%% weird joint posterior, could it be from halfnormal in prior? z_sig_gams, sig_gams, z_gams trace?
 
 az.plot_pair(trace, var_names=['gam_h', 'mu_gams', 'sig_gams'], 
          coords = {
@@ -86,16 +86,18 @@ az.plot_pair(trace, var_names=['gam_h', 'mu_gams', 'sig_gams'],
              'betas': ["b0"], 
              'sessions':['05-30'],},  kind = 'kde', marginals=True)
 
-#%% Plot curves per session and group
-sess = 25
+#%% Plot curves per session and group ??????????
+sess = 31
 sess_label = sessions[sess]
 
 param_samps = trace.posterior[['beta_vec', 'gam_h', 'gam_l']]
+#%%
 
 gam_h_samps = {}
 gam_l_samps = {}
 beta_0_samps = {}
 beta_1_samps = {}
+#%%
 
 
 for grp in ["left_bi","left_uni","right_bi","right_uni"]:
@@ -104,7 +106,7 @@ for grp in ["left_bi","left_uni","right_bi","right_uni"]:
     beta_0_samps[grp] = param_samps['beta_vec'].sel(groups = grp, sessions=sess_label, betas='b0').values.flatten()
     beta_1_samps[grp] = param_samps['beta_vec'].sel(groups = grp, sessions=sess_label, betas='b1').values.flatten()
 
-
+#%%
 def psychfunc(params, X):
     """
     Psychometric function with lapses
@@ -121,9 +123,10 @@ def psychfunc(params, X):
     gam_h, gam_l, beta0, beta1 = params
     logistic = 1 / (1 + np.exp(-(beta0 + beta1 * X)))
     return gam_h + (1 - gam_h - gam_l) * logistic
-
+#%%
 #frquencies for each level
-freq_df = pd.DataFrame({'stim': cov_mat[sess_idx==sess][:,1], 'grp_idx': grp_idx[sess_idx==sess], 'obs_data': obs_data[sess_idx==sess]})
+freq_df = pd.DataFrame({'stim': cov_mat[sess_idx==sess][:,1], 'grp_idx': grp_idx[sess_idx==sess], 'obs_data': obs_data.eval()[sess_idx==sess]})
+#%%
 freqs = pd.pivot_table(
     freq_df, 
     values='obs_data',
@@ -131,7 +134,7 @@ freqs = pd.pivot_table(
     columns='grp_idx',
     aggfunc='mean'
 )
-
+#%%
 #%%
 
 xfit = np.linspace(-1.6,1.6,500)
@@ -149,14 +152,7 @@ for grp_i, grp in enumerate(['left_uni','left_bi','right_uni','right_bi']):
     
     
     yrec[grp] = psychfunc(rec_params[grp], xfit)
-    
-    # plt.plot(xfit,yrec[grp],label='Recovered Curve',color='green')
-    # plt.fill_between(xfit, hdis[grp][:, 0], hdis[grp][:, 1], color='green', alpha=0.3, label='95% HDI')
-    # plt.scatter(np.array(freqs.index),np.array(freqs[grp_i]),label='Data', color = 'red')
-    # plt.title(grp+" "+sess_label)
-    # plt.xlabel('Stimulus Amplitude')
-    # plt.legend(loc='upper left', fontsize=9.5)
-    # plt.show()   
+ 
 
 
 #%% 
@@ -202,5 +198,206 @@ plt.title("Right Hand Psychometric Curves, Session " + sess_label)
 plt.show()     
      
 
+#%%#%% sample priors and post pred
+
+
+with model_B:
+    pm.sample_posterior_predictive(trace,extend_inferencedata=True)
+    prior = pm.sample_prior_predictive(samples=3000)
+trace.extend(prior)
+
+#%% in aggregate
+
 #%%
- 
+#PSE
+groups = coords['groups']
+fig, axes = plt.subplots(2, 2, constrained_layout=True)
+axes = axes.ravel()
+showlegend=0
+for ax, g in zip(axes, groups):
+    prior_vals = trace.prior["PSE"].sel(groups=g).values.reshape(-1)
+    post_vals  = trace.posterior["PSE"].sel(groups=g).values.reshape(-1)
+
+    # (optional) keep only finite values, safe habit
+    prior_vals = prior_vals[np.isfinite(prior_vals)]*x_sig+x_mu
+    post_vals  = post_vals[np.isfinite(post_vals)]*x_sig+x_mu
+
+    az.plot_dist(prior_vals, ax=ax, label="prior", color='blue')
+    az.plot_dist(post_vals,  ax=ax, label="posterior", color='purple')
+    #x_true = true_gam_h[g]
+    ax.axvline(28, linestyle="--", linewidth=2, label="trained threshold", color='green')
+    ax.set_xlim(20, 36)
+    ax.set_ylim(0,0.3)
+    ax.set_title(g)
+    if showlegend==0:
+        ax.legend(fontsize='small')
+        showlegend=1
+    else:
+        ax.get_legend().remove()
+fig.suptitle("PSE", fontsize=16)
+plt.show()
+
+
+#%% JND
+fig, axes = plt.subplots(2, 2, constrained_layout=True)
+axes = axes.ravel()
+showlegend=0
+for ax, g in zip(axes, groups):
+    prior_vals = trace.prior["JND"].sel(groups=g).values.reshape(-1)
+    post_vals  = trace.posterior["JND"].sel(groups=g).values.reshape(-1)
+
+    # (optional) keep only finite values, safe habit
+    prior_vals = prior_vals[np.isfinite(prior_vals)]*x_sig
+    post_vals  = post_vals[np.isfinite(post_vals)]*x_sig
+
+    az.plot_dist(prior_vals, ax=ax, label="prior", color='blue')
+    az.plot_dist(post_vals,  ax=ax, label="posterior", color='purple')
+    #x_true = true_gam_h[g]
+    #ax.axvline(28, linestyle="--", linewidth=2, label="trained threshold", color='green')
+    ax.set_xlim(0, 7)
+    ax.set_ylim(0,2.4)
+    ax.set_title(g)
+    if showlegend==0:
+        ax.legend(fontsize='small')
+        showlegend=1
+    else:
+        ax.get_legend().remove()
+fig.suptitle("JND", fontsize=16)
+plt.show()
+
+
+
+#%% gam_h
+     
+
+fig, axes = plt.subplots(2, 2, constrained_layout=True)
+axes = axes.ravel()
+showlegend=0
+for ax, g in zip(axes, groups):
+    prior_vals = trace.prior["gam_h"].sel(groups=g).values.reshape(-1)
+    post_vals  = trace.posterior["gam_h"].sel(groups=g).values.reshape(-1)
+
+    # (optional) keep only finite values, safe habit
+    prior_vals = prior_vals[np.isfinite(prior_vals)]
+    post_vals  = post_vals[np.isfinite(post_vals)]
+
+    az.plot_dist(prior_vals, ax=ax, label="prior", color='blue')
+    az.plot_dist(post_vals,  ax=ax, label="posterior", color='purple')
+    #x_true = true_gam_h[g]
+    #ax.axvline(28, linestyle="--", linewidth=2, label="trained threshold", color='green')
+    ax.set_xlim(0, 0.15)
+    ax.set_ylim(0,200)
+    ax.set_title(g)
+    if showlegend==0:
+        ax.legend(fontsize='small')
+        showlegend=1
+    else:
+        ax.get_legend().remove()
+fig.suptitle(r"$\gamma_h$", fontsize=16)
+plt.show()
+     
+#%% gam_l
+     
+
+fig, axes = plt.subplots(2, 2, constrained_layout=True)
+axes = axes.ravel()
+showlegend=0
+for ax, g in zip(axes, groups):
+    prior_vals = trace.prior["gam_l"].sel(groups=g).values.reshape(-1)
+    post_vals  = trace.posterior["gam_l"].sel(groups=g).values.reshape(-1)
+
+    # (optional) keep only finite values, safe habit
+    prior_vals = prior_vals[np.isfinite(prior_vals)]
+    post_vals  = post_vals[np.isfinite(post_vals)]
+
+    az.plot_dist(prior_vals, ax=ax, label="prior", color='blue')
+    az.plot_dist(post_vals,  ax=ax, label="posterior", color='purple')
+    #x_true = true_gam_h[g]
+    #ax.axvline(28, linestyle="--", linewidth=2, label="trained threshold", color='green')
+    ax.set_xlim(0, 0.15)
+    ax.set_ylim(0,120)
+    ax.set_title(g)
+    if showlegend==0:
+        ax.legend(fontsize='small')
+        showlegend=1
+    else:
+        ax.get_legend().remove()
+fig.suptitle(r"$\gamma_l$", fontsize=16)
+plt.show()   
+
+#%% compare unimanual vs bimanual plots
+#PSE
+
+#side, uni_grp, bi_grp = ['Right Hand', 'right_uni', 'right_bi']
+side, uni_grp, bi_grp = ['Left Hand', 'left_uni', 'left_bi']
+
+
+fig, axes = plt.subplots(1, 1, constrained_layout=True)
+ax=axes
+
+
+uni_vals  = trace.posterior["PSE"].sel(groups=uni_grp).values.reshape(-1)
+bi_vals  = trace.posterior["PSE"].sel(groups=bi_grp).values.reshape(-1)
+# (optional) keep only finite values, safe habit
+
+uni_vals  = uni_vals[np.isfinite(uni_vals)]*x_sig+x_mu
+bi_vals  = bi_vals[np.isfinite(bi_vals)]*x_sig+x_mu
+
+az.plot_dist(uni_vals,  ax=ax, label="Unimanual", color='blue')
+az.plot_dist(uni_vals,  ax=ax, label="Unimanual", color='blue', 
+             fill_kwargs={'alpha': 0.2, 'label':"95% HDI and mean"}, 
+             quantiles= [0.025, 0.5, 0.9725])
+
+
+az.plot_dist(bi_vals,  ax=ax, label="Bimanual", color='red')
+az.plot_dist(bi_vals,  ax=ax, label="Bimanual", color='red', 
+             fill_kwargs={'alpha': 0.2, 'label':"95% HDI and mean"}, 
+             quantiles= [0.025, 0.5, 0.9725])
+
+
+#x_true = true_gam_h[g]
+ax.axvline(28, linestyle="--", linewidth=2, label="trained threshold", color='green')
+#ax.set_xlim(22, 34)
+#ax.set_ylim(0,1.7)
+ax.set_title(side)
+ax.legend(fontsize='small', loc='upper center')
+fig.suptitle("Posterior Estimates: PSE", fontsize=16)
+plt.show()
+
+#%% JND
+     
+
+#side, uni_grp, bi_grp = ['Right Hand', 'right_uni', 'right_bi']
+side, uni_grp, bi_grp = ['Left Hand', 'left_uni', 'left_bi']
+
+fig, axes = plt.subplots(1, 1, constrained_layout=True)
+ax=axes
+
+
+uni_vals  = trace.posterior["JND"].sel(groups=uni_grp).values.reshape(-1)
+bi_vals  = trace.posterior["JND"].sel(groups=bi_grp).values.reshape(-1)
+# (optional) keep only finite values, safe habit
+
+uni_vals  = uni_vals[np.isfinite(uni_vals)]*x_sig
+bi_vals  = bi_vals[np.isfinite(bi_vals)]*x_sig
+
+az.plot_dist(uni_vals,  ax=ax, label="Unimanual", color='blue')
+az.plot_dist(uni_vals,  ax=ax, label="Unimanual", color='blue', 
+             fill_kwargs={'alpha': 0.3, 'label':"95% HDI and mean"}, 
+             quantiles= [0.025, 0.5, 0.9725])
+
+
+az.plot_dist(bi_vals,  ax=ax, label="Bimanual", color='red')
+az.plot_dist(bi_vals,  ax=ax, label="Bimanual", color='red', 
+             fill_kwargs={'alpha': 0.3, 'label':"95% HDI and mean"}, 
+             quantiles= [0.025, 0.5, 0.9725])
+
+
+#x_true = true_gam_h[g]
+#ax.axvline(28, linestyle="--", linewidth=2, label="trained threshold", color='green')
+ax.set_xlim(0, 20)
+#ax.set_ylim(0,1.7)
+ax.set_title(side)
+ax.legend(fontsize='small')
+fig.suptitle("Posterior Estimates: JND", fontsize=16)
+plt.show()
